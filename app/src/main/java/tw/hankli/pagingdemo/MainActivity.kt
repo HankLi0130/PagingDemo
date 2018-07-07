30e6b37562ac5b21d1ff0a09bc3e0505006df1ca
Hank Li <hankli0130@gmail.com>
2018-07-07 20:10:31 +0800

    加入MainAdapter

--- app/src/main/java/tw/hankli/pagingdemo/MainActivity.kt
+++ app/src/main/java/tw/hankli/pagingdemo/MainActivity.kt
@@ -3,15 +3,21 @@ package tw.hankli.pagingdemo
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by lazy(LazyThreadSafetyMode.NONE) {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
+    private val adapter: MainAdapter = MainAdapter(emptyList())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        view_item_list.setHasFixedSize(true)
+        view_item_list.adapter = adapter
     }
 }
---
+++ app/src/main/java/tw/hankli/pagingdemo/MainAdapter.kt
@@ -0,0 +1,30 @@
+package tw.hankli.pagingdemo
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.layout_item.view.*
+import tw.hankli.pagingdemo.models.Item
+
+class MainAdapter(var items: List<Item>) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(item: Item) {
+            itemView.view_id.text = item.id.toString()
+            itemView.view_name.text = item.name
+        }
+    }
+}
