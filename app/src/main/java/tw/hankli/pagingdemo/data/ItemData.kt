4079930457ad105f041c0d19ce4c2874bc823ef8
Hank Li <hankli0130@gmail.com>
2018-07-08 16:06:39 +0800

    ItemDataSourceFactory可選擇建立的DataSource

--- app/src/main/java/tw/hankli/pagingdemo/data/ItemData.kt
+++ app/src/main/java/tw/hankli/pagingdemo/data/ItemData.kt
@@ -8,6 +8,7 @@ import tw.hankli.pagingdemo.models.Item
 
 object ItemData {
 
+    // 累加
     fun getIncreaseItems(start: Int, size: Int): List<Item> {
         return Array(size) {
             val index = start + it
@@ -15,6 +16,7 @@ object ItemData {
         }.toList()
     }
 
+    // 累減，陣列的第一個元素是最小的數
     fun getReduceItems(start: Int, size: Int): List<Item> {
         return Array(size) {
             val index = (start - size) + it
--- app/src/main/java/tw/hankli/pagingdemo/paging/ItemDataSourceFactory.kt
+++ app/src/main/java/tw/hankli/pagingdemo/paging/ItemDataSourceFactory.kt
@@ -8,10 +8,25 @@ class ItemDataSourceFactory : DataSource.Factory<Int, Item>() {
 
     private val tag = this::class.java.simpleName
 
+    companion object {
+
+        const val ITEM_KEYED = 0
+        const val PAGE_KEYED = 1
+        const val POSITIONAL = 2
+
+        fun getDataSource(which: Int): DataSource<Int, Item> {
+
+            return when (which) {
+                ITEM_KEYED -> MyItemKeyedDataSource()
+                else -> throw IllegalArgumentException("unknown data source")
+            }
+        }
+    }
+
     override fun create(): DataSource<Int, Item> {
 
         Log.i(tag, "create data source factory")
 
-        return MyItemKeyedDataSource()
+        return getDataSource(ITEM_KEYED)
     }
 }
